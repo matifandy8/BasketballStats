86bcfce33a9c483852320f688be647e7733df1e6
Matias Fandiño <matifandy@gmail.com>
2025-09-03 16:22:37 -0300

    fix: fix build

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
-
   ],
+  build: {
+    outDir: 'public', // Cambia 'dist' por 'public' si tu host lo requiere
+  },
 })
